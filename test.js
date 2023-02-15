3f46330e4e3c5c0cdc217a52b7cd4089d265cf2d
Tahseen Dar <tahseendar01@gmail.com>
2023-02-16 01:21:00 +0500

    fixed errors

--- test.js
+++ test.js
@@ -16,25 +16,26 @@ tape("should respond hello", (t) => {
   });
 });
 
-tape("should respond user-agent"),
-  (t) => {
-    const opts = { headers: { "User-Agent": "tape" } };
-    jsonist.get(`${urlBase}/user-agent`, opts, (err, body) => {
-      if (err) t.error(err);
+tape("should respond user-agent", (t) => {
+  const opts = { headers: { "User-Agent": "tape" } };
+  jsonist.get(`${urlBase}/user-agent`, opts, (err, body) => {
+    if (err) t.error(err);
 
-      t.equal(body.ua, "tape");
-      tape("should respond b64", (t) => {
-        jsonist.get(`${urlBase}/b64/hello`, (err, body) => {
-          if (err) t.error(err);
+    t.equal(body.ua, "tape");
+    t.end();
+  });
+});
 
-          t.equal(body.b64, "aGVsbG8=");
-          t.end();
-        });
-      });
+tape("should respond b64", (t) => {
+  jsonist.get(`${urlBase}/b64/hello`, (err, body) => {
+    if (err) t.error(err);
 
-      tape("cleanup", function (t) {
-        server.close();
-        t.end();
-      });
-    });
-  };
+    t.equal(body.b64, "aGVsbG8=");
+    t.end();
+  });
+});
+
+tape("cleanup", function (t) {
+  server.close();
+  t.end();
+});
